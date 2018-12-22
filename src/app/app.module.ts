09d18b60c23a1e71461b2fe4dcdc4038fe2d4531
Eashwaran Raghu <eashwaranraghu@gmail.com>
2018-12-23 01:24:53 +0530

    removing env

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,24 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { AngularFireModule } from '@angular/fire';
-import { environment } from '../environments/environment';
 
+import { AngularFireModule } from '@angular/fire';
+import {AngularFireStorageModule} from '@angular/fire/storage';
+import {AngularFireDatabaseModule} from '@angular/fire/database';
+import { environment } from '../environments/environment';
 
 import { AppRoutingModule } from './app.routing';
 import { ComponentsModule } from './components/components.module';
 
 import { AppComponent } from './app.component';
-
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
 
 @NgModule({
   imports: [
     BrowserAnimationsModule,
       AngularFireModule.initializeApp(environment.firebase),
+      AngularFireStorageModule,
+      AngularFireDatabaseModule,
     FormsModule,
     HttpModule,
     ComponentsModule,
