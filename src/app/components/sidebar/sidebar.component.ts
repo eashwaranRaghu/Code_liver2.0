7f45a73d70ec1a58c4a4225817a53b54dab14228
prashantraghu999 <prashantraghu999@gmail.com>
2018-12-26 03:17:43 +0530

    nothing left to do now

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -13,7 +13,7 @@ export const ROUTES: RouteInfo[] = [
     { path: '/filesharing', title: 'DropZone',  icon:'files_single-copy-04', class: '' },
     { path: '/spacerocker', title: 'Chess Vs AI',  icon:'media-1_button-play', class: '' },
     { path: '/about', title: 'About',  icon:'travel_info', class: '' },
-    { path: '/help', title: 'Help',  icon:'education_agenda-bookmark', class: '' },
+   /* { path: '/help', title: 'Help',  icon:'education_agenda-bookmark', class: '' },
 
     { path: '/dashboard', title: 'Editor1',  icon: 'design_app', class: '' },
     { path: '/icons', title: 'Rooms1',  icon:'business_bank', class: '' },
@@ -21,7 +21,7 @@ export const ROUTES: RouteInfo[] = [
     { path: '/notifications', title: 'SpaceRocker1',  icon:'media-1_button-play', class: '' },
     { path: '/user-profile', title: 'File Sharing1',  icon:'files_single-copy-04', class: '' },
     { path: '/table-list', title: 'About1',  icon:'travel_info', class: '' },
-    { path: '/typography', title: 'Help1',  icon:'text_caps-small', class: '' }
+    { path: '/typography', title: 'Help1',  icon:'text_caps-small', class: '' }*/
 ];
 
 @Component({
--- src/app/file-sharing/file-sharing.component.ts
+++ src/app/file-sharing/file-sharing.component.ts
@@ -3,32 +3,117 @@ import {AngularFireStorage} from '@angular/fire/storage';
 import {Observable} from 'rxjs/Rx';
 import {finalize} from 'rxjs/operators';
 
+import {DataService} from '../data.service';
+import * as moment from 'moment';
 @Component({
     selector: 'app-file-sharing',
     templateUrl: './file-sharing.component.html',
     styleUrls: ['./file-sharing.component.scss']
 })
 export class FileSharingComponent implements OnInit {
+    filesarr;files;
+   dropArea;drop_txt="Drop Files Here";back="transparent";
+
+
+
+
+   done='';
     uploadPercent: Observable<number>;
     downloadURL: Observable<string>;
-    constructor(private storage: AngularFireStorage) { }
+    constructor(private storage: AngularFireStorage, public data: DataService) {
+        this.files = data.db.list('files');
+        this.filesarr = data.db.list('files').valueChanges(); //|async
+        this.filesarr.subscribe(a => console.log(a));
+         /*this.dropArea.addEventListener('dragover', handlerFunction, false)
+          this.dropArea.addEventListener('drop', handlerFunction, false) */
+      }eurl;
     uploadFile(event) {
         const file = event.target.files[0];
-        const filePath = 'name-your-file-path-here';
+ this.done='';
+         console.log(event);
+        const size = parseInt(file.size)/1000000; //store
+        console.log(file.name);                //store
+        const filePath = `test/${new Date().getTime()}_${file.name}`;
+        console.log(filePath);
         const fileRef = this.storage.ref(filePath);
         const task = this.storage.upload(filePath, file);
-
         // observe percentage changes
         this.uploadPercent = task.percentageChanges();
         // get notified when the download URL is available
         task.snapshotChanges().pipe(
-            finalize(() => this.downloadURL = fileRef.getDownloadURL() )
+            finalize(() => {this.downloadURL = fileRef.getDownloadURL();
+                this.downloadURL.subscribe(e => {this.eurl = e;console.log(e)
+                    this.files.push({ name: file.name, size: size, url: e });});
+                console.log(this.downloadURL);this.done='done';    //store
+              
+            })
+        )
+            .subscribe()
+
+    }
+     uploadFile3(event){
+         this.done='';
+         console.log(event);
+        const file = event.item(0); //just change to this
+        const size = parseInt(file.size)/1000000; //store
+        console.log(file.name);                //store
+        const filePath = `test/${new Date().getTime()}_${file.name}`;
+        console.log(filePath);
+        const fileRef = this.storage.ref(filePath);
+        const task = this.storage.upload(filePath, file);
+        // observe percentage changes
+        this.uploadPercent = task.percentageChanges();
+        // get notified when the download URL is available
+        task.snapshotChanges().pipe(
+            finalize(() => {this.downloadURL = fileRef.getDownloadURL();
+                this.downloadURL.subscribe(e => {this.eurl = e;console.log(e)
+                    this.files.push({ name: file.name, size: size, url: e });});
+                console.log(this.downloadURL);this.done='done';    //store
+              
+            })
         )
             .subscribe()
     }
     ngOnInit() {
+         this.dropArea = document.getElementById('drop-area');
+         ;['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
+              this.dropArea.addEventListener(eventName, this.preventDefaults, false)
+            })
+         this.dropArea.addEventListener('dragleave', (e)=> {console.log(e); this.toggle_style();}, false)
+         this.dropArea.addEventListener('dragenter', (e)=> {console.log(e); this.toggle_style();}, false)
+         
+         this.dropArea.addEventListener('drop',  (e)=> {this.toggle_style();}, false)
     }
+  preventDefaults(e){
+  e.preventDefault()
+  e.stopPropagation()
+   }bol=true;
+   toggle_style(){
+       if(this.drop_txt=="Drop Files Here"){
+           this.bol = false;
+          this.back = "#9d3f91";
+          this.drop_txt="Here comes The Most Awaited File";
+       }
+       else if(this.drop_txt=="Here comes The Most Awaited File"){
+           this.bol = true;
+          this.back="transparent"
+          this.drop_txt="Drop Files Here";
+       }
 
+   }
+   handleFiles(files) {
+      //([...files]).forEach(this.uploadFile2)
+}
+task;percentage;snapshot;
+  // State for dropzone CSS toggling
+  isHovering: boolean;
+  toggleHover(event: boolean) {
+    this.isHovering = event;
+  }
+  // Determines if the upload task is active
+  isActive(snapshot) {
+    return snapshot.state === 'running' && snapshot.bytesTransferred < snapshot.totalBytes
+  }
 }
 
 // https://github.com/angular/angularfire2/blob/master/docs/storage/storage.md
