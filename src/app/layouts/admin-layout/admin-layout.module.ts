b2cc24165161579db358536fe78e017a00ffec0d
prashantraghu999 <prashantraghu999@gmail.com>
2018-12-26 11:35:05 +0530

    Final Commit

--- src/app/layouts/admin-layout/admin-layout.module.ts
+++ src/app/layouts/admin-layout/admin-layout.module.ts
@@ -3,13 +3,6 @@ import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AdminLayoutRoutes } from './admin-layout.routing';
-import { DashboardComponent } from '../../dashboard/dashboard.component';
-import { UserProfileComponent } from '../../user-profile/user-profile.component';
-import { TableListComponent } from '../../table-list/table-list.component';
-import { TypographyComponent } from '../../typography/typography.component';
-import { IconsComponent } from '../../icons/icons.component';
-import { MapsComponent } from '../../maps/maps.component';
-import { NotificationsComponent } from '../../notifications/notifications.component';
 import { ChartsModule } from 'ng2-charts';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ToastrModule } from 'ngx-toastr';
@@ -21,7 +14,6 @@ import { VideoChatComponent } from '../../video-chat/video-chat.component';
 import { SpacerockerComponent } from '../../spacerocker/spacerocker.component';
 import { FileSharingComponent } from '../../file-sharing/file-sharing.component';
 import { AboutComponent } from '../../about/about.component';
-import { HelpComponent } from '../../help/help.component';
 import { SafePipe } from '../../safe.pipe';
 import {DropZoneDirective} from '../../drop-zone.directive';
 import {FileSizePipe} from '../../file-size.pipe';
@@ -37,20 +29,12 @@ import {StorageBucket} from '@angular/fire/storage';
     ToastrModule.forRoot()
   ],
   declarations: [
-    DashboardComponent,
-    UserProfileComponent,
-    TableListComponent,
-    TypographyComponent,
-    IconsComponent,
-    MapsComponent,
-    NotificationsComponent,
     EditorComponent,
     RoomsComponent,
     VideoChatComponent,
     SpacerockerComponent,
     FileSharingComponent,
     AboutComponent,
-    HelpComponent,
     SafePipe,
     DropZoneDirective,
     FileSizePipe
--- src/app/layouts/admin-layout/admin-layout.routing.ts
+++ src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -1,11 +1,4 @@
 import { Routes } from '@angular/router';
-import { DashboardComponent } from '../../dashboard/dashboard.component';
-import { UserProfileComponent } from '../../user-profile/user-profile.component';
-import { TableListComponent } from '../../table-list/table-list.component';
-import { TypographyComponent } from '../../typography/typography.component';
-import { IconsComponent } from '../../icons/icons.component';
-import { MapsComponent } from '../../maps/maps.component';
-import { NotificationsComponent } from '../../notifications/notifications.component';
 
 import { EditorComponent } from '../../editor/editor.component';
 import { RoomsComponent } from '../../rooms/rooms.component';
@@ -13,22 +6,11 @@ import { VideoChatComponent } from '../../video-chat/video-chat.component';
 import { SpacerockerComponent } from '../../spacerocker/spacerocker.component';
 import { FileSharingComponent } from '../../file-sharing/file-sharing.component';
 import { AboutComponent } from '../../about/about.component';
-import { HelpComponent } from '../../help/help.component';
-
 export const AdminLayoutRoutes: Routes = [
     { path: 'editor',      component: EditorComponent },
     { path: 'rooms',   component: RoomsComponent },
     { path: 'video-chat',     component: VideoChatComponent },
     { path: 'spacerocker',     component: SpacerockerComponent },
     { path: 'filesharing',          component: FileSharingComponent },
-    { path: 'about',           component: AboutComponent },
-    { path: 'help',  component: HelpComponent },
-
-    { path: 'dashboard',      component: DashboardComponent },
-    { path: 'user-profile',   component: UserProfileComponent },
-    { path: 'table-list',     component: TableListComponent },
-    { path: 'typography',     component: TypographyComponent },
-    { path: 'icons',          component: IconsComponent },
-    { path: 'maps',           component: MapsComponent },
-    { path: 'notifications',  component: NotificationsComponent },
+    { path: 'about',           component: AboutComponent }
 ];
